0d5af4240182e3b0679e9ac6bc60d6b2db980984
unknown <christianlaguyo5@gmail.com>
2023-11-13 13:50:57 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Create from "./Create";
+import Home from "./Home";
+import Read from "./Read";
+import Update from "./Update";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/read/:id" element={<Read />} />
+        <Route path="/edit/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
